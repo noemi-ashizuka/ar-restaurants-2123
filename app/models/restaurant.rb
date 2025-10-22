1e5232dcfb43ed378993ebfa18e687f69b287db7
noemi ashizuka <noemi.calanducci@gmail.com>
2025-10-22 09:51:55 +0900

    empty boilerplate

--- app/models/restaurant.rb
+++
@@ -1,5 +0,0 @@
-# table restaurants -> model Restaurant
-# projects -> Project
-class Restaurant < ActiveRecord::Base
-  # attr_accessor :address => no need, AR gives us accessors on each column
-end
