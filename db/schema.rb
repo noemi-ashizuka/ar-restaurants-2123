f2601f77255d4c99b70feeb007293ce76918ee79
noemi ashizuka <noemi.calanducci@gmail.com>
2025-10-23 08:45:05 +0900

    cleaned boilerplate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_24_094247) do
-  create_table "restaurants", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "rating", default: 0
-  end
-
+ActiveRecord::Schema[7.1].define(version: 0) do
 end
